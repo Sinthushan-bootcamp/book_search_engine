79807f1b97bf1b7e7a4d6fa4837f7a333468fe0d
sinthushan <sinthushan@gmail.com>
2023-05-20 12:36:59 -0400

    finished backend schema

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,7 +1,37 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Integer
+    savedBooks: [Book]
+  }
   
+  type Book {
+    bookId:String
+    authors: [Book]
+    description:String
+    title:String
+    image:String
+    link:String
+  }
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    addUser(username,: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    saveBook(profileId: ID!, skill: String!): Profile
+    removeBook(skill: String!): Profile
+  }
 `;
 
 module.exports = typeDefs;
